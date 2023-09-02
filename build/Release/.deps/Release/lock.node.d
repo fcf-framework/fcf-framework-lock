ab9e3fe814da7fc79856d52684f0cf4eaf23ac41
Vladimir Markin <ghiowik@gmail.com>
2023-09-02 03:16:15 +0300

    Version 1.0.3

--- build/Release/.deps/Release/lock.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/lock.node := ln -f "Release/obj.target/lock.node" "Release/lock.node" 2>/dev/null || (rm -rf "Release/lock.node" && cp -af "Release/obj.target/lock.node" "Release/lock.node")
--- build/Release/.deps/Release/obj.target/lock.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/lock.node := g++ -o Release/obj.target/lock.node -shared -pthread -rdynamic -m64  -Wl,-soname=lock.node -Wl,--start-group Release/obj.target/lock/lock.o -Wl,--end-group 
